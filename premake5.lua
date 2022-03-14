a0dd32e73057d84c5ca8da91adaaa5e29eb8d4aa
XunHang <fma1995@163.com>
2022-03-14 10:17:40 +0800

    Using premake to generate Solution.

---
+++ premake5.lua
@@ -0,0 +1,98 @@
+workspace "D_Hero_Prj"
+	architecture "x86_64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+OUT_DIR = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "D_Hero_Prj"
+	location "D_Hero_Prj"
+	kind "SharedLib"
+	language "C++"
+	
+	targetdir ("bin/" .. OUT_DIR .. "./%{prj.name}")
+	objdir ("bin-int/" .. OUT_DIR .. "./%{prj.name}")
+
+	files {
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs {
+		"%{prj.name}/ThirdParty/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "c++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines {
+			"DH_PLATFORM_WINDOWS",
+			"DH_BUILD_DLL"
+		}
+
+		postbuildcommands {
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. OUT_DIR .. "/SandBox")
+		}
+
+	filter "configurations:Debug"
+		defines "DH_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "DH_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "DH_DIST"
+		optimize "On"
+
+
+project "SandBox"
+	location "SandBox"
+	kind "ConsoleApp"
+	language "C++"
+	
+	targetdir ("bin/" .. OUT_DIR .. "./%{prj.name}")
+	objdir ("bin-int/" .. OUT_DIR .. "./%{prj.name}")
+
+	files {
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs {
+		"D_Hero_Prj/ThirdParty/spdlog/include",
+		"D_Hero_Prj/src"
+	}
+
+	links {
+		"D_Hero_Prj"
+	}
+
+	filter "system:windows"
+		cppdialect "c++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines {
+			"DH_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "DH_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "DH_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "DH_DIST"
+		optimize "On"
